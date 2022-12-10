218baa8f4e600ac019252fd298d37f8a0ec830fc
Debanjana <debanjana.9806@gmail.com>
2022-12-09 21:36:33 -0800

    submit

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
 //your code here
-function hover(){
-    document.getElementById('btn').style.display='block'
-    
-}
-// function hoverOut(){
-//     document.getElementById('btn').style.background='black'
-//     document.getElementById('btn').style.color='white'
-//     document.getElementById('btn').style.borderRadius='5px'
-// }
